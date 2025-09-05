4909843883b60029cc380aef3e31056ef20041ba
Rohan Khurana <rohan.khurana.dev.iic@gmail.com>
2025-09-05 16:52:03 +0530

    react router dom added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import { Button } from "./components/ui/button"
+// App.tsx
+import { Link, Route, Routes } from "react-router-dom";
 
-
-function App() {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Snake n ladder</Button>
-    </div>
-  )
+function Home() {
+  return <h2>Home Page</h2>;
 }
 
-export default App
+function About() {
+  return <h2>About Page</h2>;
+}
+
+function Contact() {
+  return <h2>Contact Page</h2>;
+}
+
+export default function App() {
+  return (
+    <div>
+      <nav className="flex gap-4 p-4 bg-gray-100">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <div className="p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
